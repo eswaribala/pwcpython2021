8488e5d9b73ef4788c9dd56c85db8a836d2179c2
eswaribala <parameswaribala@gmail.com>
2021-07-07 16:39:32 +0530

    updated

--- databases/storedata.py
+++ databases/storedata.py
@@ -6,6 +6,22 @@ class DBTest:
                    passwd="vignesh",
                    db="pwcdb")
 
+    def addProduct(self, data):
 
+        cursor = self.conn.cursor();
+        print("Cursor ready...");
+        try:
+            cursor.execute("""insert into product values 
+        ('%d','%s','%s','%d')""" % (data[0], data[1], data[2],data[3]));
+            # cursor.execute(query);
+            self.conn.commit()
+        except pymysql.Error as e:
+            print("Exception occurred", e)
+            self.conn.rollback()
+        finally:
+            cursor.close();
+            self.conn.close();
 
 dbTest=DBTest()
+data=[2423,'Mouse','2020-1-2',1500]
+dbTest.addProduct(data)
